f5d6e0aeab2c6a4df4db024d1872b2e2cdc43bd3
leloup314 <s6plwolf@uni-bonn.de>
2016-08-26 01:31:45 +0200

    BUG: incorrect bitwise AND caused wrong fe_trigger number

--- pyBAR_mimosa26_interpreter/correlation_functions.py
+++ pyBAR_mimosa26_interpreter/correlation_functions.py
@@ -1,5 +1,6 @@
 from numba import njit
 import numpy as np
+import time
 
 m26_dtype = np.dtype([('event_number', '<i8'),('timestamp', '<u4'), ('trigger_number_begin', '<u2'),('trigger_number_end', '<u2'), 
                       ('plane', '<u2'), ('frame', '<u4'), ('column', '<u2'), ('row', '<u4'),('trigger_status', '<u2'), ('event_status', '<u2')])
@@ -49,19 +50,27 @@ def correlate_fm(fe_data, m26_data, corr_col, corr_row, dut1, dut2, transpose = 
                 m26_index += 1 # skip here
                 continue
             
-        while fe_data[fe_index]['trigger_number'] & 0xFFFF < m26_trigger_begin: # keep the fe trigger up with the beginning of the m26 trigger range to set fe_index to starting trigger
+        while fe_data[fe_index]['trigger_number'] & 0x7FFF < m26_trigger_begin: # keep the fe trigger up with the beginning of the m26 trigger range to set fe_index to starting trigger
                 if fe_index == fe_data.shape[0]-1:
                     return fe_index, m26_index
-                elif fe_data[fe_index]['trigger_number'] & 0xFFFF == m26_trigger_begin: # break if fe_index matches the right trigger
+                elif fe_data[fe_index]['trigger_number'] & 0x7FFF == m26_trigger_begin: # break if fe_index matches the right trigger
                     break
                 else:
                     fe_index += 1
-
+                    
+        if fe_data[fe_index]['trigger_number'] & 0x7FFF > m26_trigger_begin: # keep up trigger range of mimosa with fe trigger
+            if m26_index == m26_data.shape[0]-1:
+                return fe_index, m26_index
+            else:
+                m26_index += 1
+                continue
+        
+        
         ###search trigger number in  fe_data
         fe_buf_index=0 # overwrite buffer
         for fe_i in range(fe_index, fe_data.shape[0]):    
                 
-            fe_trigger = fe_data[fe_i]['trigger_number'] & 0xFFFF # get trigger number of current index fe_i
+            fe_trigger = fe_data[fe_i]['trigger_number'] & 0x7FFF # get trigger number of current index fe_i
 
             if m26_trigger_begin <= m26_trigger_end: # normal case, no trigger overflow
                 
@@ -124,106 +133,104 @@ def correlate_fm(fe_data, m26_data, corr_col, corr_row, dut1, dut2, transpose = 
     
     
 @njit
-def correlate_fm_beta(fe_data, m26_data, corr_col, corr_row, dut1, dut2, transpose = True): #beta for testing
+def correlate_fm_beta(fe_data, m26_data, corr_col, corr_row, dut1, dut2, transpose = True): #beta; do not consider trigger overflow
     #initialise variables
     #fei4
-    #fe_index = 0
-    fe_start = 0
-    fe_buf_index = 0
-    fe_buf_col = np.zeros(1000, dtype=np.uint32) #make buffer for column correlation
-    fe_buf_row = np.zeros(1000, dtype=np.uint32) #make buffer for row correlation
+    fe_index = 0 # index
+    fe_start = 0 # starting index
+    fe_check = 0 # index to check whether correlation is possible within mimosa trigger range for current fe data
     #m26
-    m26_index = 0
+    m26_index = 0 # index
     #end of initialisation
     
-    while m26_index < m26_data.shape[0]:
+    if m26_data.shape[0] == 0 or fe_data.shape[0] == 0: # skip empty data
+        return fe_start, m26_index
+    
+    while m26_index < m26_data.shape[0]: # main loop
         
-        m26_frame = m26_data[m26_index]['frame']
-        m26_trigger_begin = m26_data[m26_index]['trigger_number_begin']
-        m26_trigger_end = m26_data[m26_index]['trigger_number_end']
-        
-        if m26_trigger_begin ==0xFFFF or m26_trigger_end == 0xFFFF: #skip frames with no trigger range in mimosa data (default==0xFFFF)
-            if m26_index == m26_data.shape[0]-1:
-                return fe_start, m26_index
-            else:
-                m26_index += 1
-                continue
-        
-        while fe_data[fe_start]['trigger_number'] & 0xFFFF < m26_trigger_begin:
-                if fe_start == fe_data.shape[0]-1:
-                    return fe_start, m26_index
-                else:
-                    fe_start += 1
-        
-        fe_buf_index = 0
-        for fe_i in range(fe_start, fe_data.shape[0]):
-            
-            fe_trigger = fe_data[fe_i]['trigger_number'] & 0xFFFF
-            
-            if fe_trigger < m26_trigger_begin:
-                continue
-                
-            if fe_trigger == m26_trigger_begin:
-                fe_start = fe_i
-            
-            if m26_trigger_begin <= m26_trigger_end: # normal case
-                
-                if fe_trigger >= m26_trigger_begin and fe_trigger <= m26_trigger_end:
-                        fe_buf_col[fe_buf_index] = fe_data[fe_i]['column']
-                        fe_buf_row[fe_buf_index] = fe_data[fe_i]['row'] 
-                        fe_buf_index += 1
-                        
-                elif ((m26_trigger_end - fe_trigger) & 0x7FFF ) > 0x4000: #if fe_trigger is close to overflow and m26_trigger_end is small, we dont want to break because this fe_data belongs to this m26_data
-                    break
-            
-            else:  # overflow of m26 trigger end
-                
-                if (fe_trigger >= m26_trigger_begin and fe_trigger <= 0x7FFF) or (fe_trigger <= m26_trigger_end and fe_trigger >= 0):        
-                        fe_buf_col[fe_buf_index] = fe_data[fe_i]['column']
-                        fe_buf_row[fe_buf_index] = fe_data[fe_i]['row'] 
-                        fe_buf_index += 1
-                
-                elif ((m26_trigger_end-fe_trigger) & 0x7FFF ) > 0x4000:  #if fe_trigger is close to overflow and m26_trigger_end is small, we dont want to break because this fe_data belongs to this m26_data
-                    break
-        
-        if fe_i == fe_data.shape[0]-1:
-            return fe_start, m26_index
+        m26_frame = m26_data[m26_index]['frame'] # get mimosas frame number
         
         for m26_i in range(m26_index, m26_data.shape[0]):
             
-            if m26_data[m26_i]['trigger_number_begin'] == 0xFFFF or m26_data[m26_i]['trigger_number_end'] == 0xFFFF:
-                continue
-                
-            if m26_frame == m26_data[m26_i]['frame']:
-                
-                for i in range(fe_buf_index): # fill histogramms
-                    
-                    if transpose == True: # m26_col corresponds to fe_row and m26_row corresponds to fe_col because of our geometry of telescope
-                        
-                        if dut1 == 0: # correlate fe to m26
-                            corr_col[fe_buf_row[i], m26_data[m26_i]['column']] += 1 
-                            corr_row[fe_buf_col[i], m26_data[m26_i]['row']] += 1    
-                        elif dut2 == 0: # correlate m26 to fe
-                            corr_col[m26_data[m26_i]['column'],fe_buf_row[i]] += 1 
-                            corr_row[m26_data[m26_i]['row'],fe_buf_col[i]] += 1 
-                    
-                    else: #m26_col corresponds to fe_col and m26_row corresponds to fe_row
-                        
-                        if dut1 == 0: # correlate fe to m26
-                            corr_col[fe_buf_col[i], m26_data[m26_i]['column']] += 1 
-                            corr_row[fe_buf_row[i], m26_data[m26_i]['row']] += 1     
-                        elif dut2 == 0: # correlate m26 to fe
-                            corr_col[m26_data[m26_i]['column'],fe_buf_col[i]] += 1 
-                            corr_row[m26_data[m26_i]['row'],fe_buf_row[i]] += 1 
-            else:
+            if m26_frame != m26_data[m26_i]['frame']: # do not correlate if frames are not equal
                 break
-                
-        if m26_i== m26_data.shape[0]-1: #end of m26_data, in this case m26_data is to short for corresponding fe_data; add new data to fe_buffer and start from previous fe_index; start from m26_i because needed m26_data < m26_i is already in histogramm 
-            return fe_start, m26_i
             
-        m26_index = m26_i
+            m26_trigger_begin  = m26_data[m26_i]['trigger_number_begin'] # get mimosa trigger range
+            m26_trigger_end = m26_data[m26_i]['trigger_number_end']
+            
+            
+            if m26_trigger_begin == 0xFFFF or m26_trigger_end == 0xFFFF or m26_trigger_begin == 0 or m26_trigger_end == 0: # if mimosa frame has no trigger, trigger begin and end will be 0xFFFF; skip since we can not correlate; also skip 0 since we do not ant to deal with overflow
+                if m26_i == m26_data.shape[0]-1:
+                    return fe_start, m26_i
+                else:
+                    continue
+            
+            if m26_trigger_begin > m26_trigger_end: # trigger overflow will be skipped here
+                if m26_i == m26_data.shape[0]-1:
+                    return fe_start, m26_i
+                else:
+                    continue
+            
+            
+            while fe_data[fe_start]['trigger_number'] & 0x7FFF < m26_trigger_begin: # keep up fe trigger with mimosa trigger range
+                if fe_start == fe_data.shape[0]-1:
+                    return fe_start, m26_i
+                elif fe_data[fe_start]['trigger_number'] & 0x7FFF == m26_trigger_begin:
+                    break
+                else:
+                    fe_start += 1
+                    
+            if fe_data[fe_start]['trigger_number'] & 0x7FFF > m26_trigger_begin: # keep up mimosa trigger range with fe trigger
+                if m26_i == m26_data.shape[0]-1:
+                    return fe_start, m26_i 
+                else:
+                    continue
+                    
+            fe_index = fe_start # set fe_index to correct starting index
+            fe_check = fe_start # set fe_check to correct starting index
+            
+            while fe_data[fe_check]['trigger_number'] & 0x7FFF >= m26_trigger_begin and fe_data[fe_check]['trigger_number'] & 0x7FFF <= m26_trigger_end: # check whether correlation for current mimosa trigger range can be done within length of fe data; if not return; do this because we directly correlate and do not buffer
+                if fe_check == fe_data.shape[0]-1:
+                    return fe_start, m26_i
+                else:
+                    fe_check += 1
+
+                    
+            while fe_data[fe_index]['trigger_number'] & 0x7FFF >= m26_trigger_begin and fe_data[fe_index]['trigger_number'] & 0x7FFF <= m26_trigger_end: # correlate
+                
+                if transpose == True: # m26_col corresponds to fe_row and m26_row corresponds to fe_col because of our geometry of our telescope
+                    
+                    if dut1 == 0: # correlate fe to m26
+                        corr_col[fe_data[fe_index]['row'], m26_data[m26_i]['column']] += 1 
+                        corr_row[fe_data[fe_index]['column'], m26_data[m26_i]['row']] += 1    
+                    elif dut2 == 0: # correlate m26 to fe
+                        corr_col[m26_data[m26_i]['column'],fe_data[fe_index]['row']] += 1 
+                        corr_row[m26_data[m26_i]['row'],fe_data[fe_index]['column']] += 1 
+                
+                else: #m26_col corresponds to fe_col and m26_row corresponds to fe_row
+                    
+                    if dut1 == 0: # correlate fe to m26
+                        corr_col[fe_data[fe_index]['column'], m26_data[m26_i]['column']] += 1 
+                        corr_row[fe_data[fe_index]['row'], m26_data[m26_i]['row']] += 1     
+                    elif dut2 == 0: # correlate m26 to fe
+                        corr_col[m26_data[m26_i]['column'],fe_data[fe_index]['column']] += 1 
+                        corr_row[m26_data[m26_i]['row'],fe_data[fe_index]['row']] += 1 
+                
+                fe_index += 1
         
-    return -1, -1
+        if m26_i == m26_data.shape[0]-1: # if end of mimosa data is reached
+            return fe_start, m26_i
+        
+        m26_index = m26_i # set new starting point
+            
+    return -1, -1 
+        
+                
+                        
+                
+                        
+                    
+    
         
 
 
